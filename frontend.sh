44d0f342c065821f240d9ed2b52d3ce23fb7c551
mahipal <mahipal.muchkurr@gmail.com>
2023-07-28 16:47:16 +0530

    Roboshop Scripts Update DNS Records edited systemctl

--- frontend.sh
+++ frontend.sh
@@ -9,3 +9,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+systemctl enable nginx
+systemctl restart nginx ; tail -f /var/log/messages
