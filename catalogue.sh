9cc89d7f2aa1fd7528b9614659d721cc08930213
mahipal <mahipal.muchkurr@gmail.com>
2023-07-28 16:05:54 +0530

    Roboshop Scripts Update DNS Records

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ cd /app
 npm install
 
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mongodb.mdevopsb74.online </app/schema/catalogue.js
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
