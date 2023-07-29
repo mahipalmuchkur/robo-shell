247db24fd54dfe23b9f4a4b799993182cc2a58ff
mahipal <mahipal.muchkurr@gmail.com>
2023-07-29 21:30:44 +0530

    Echos - Placing headings

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,16 @@
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo ">>>>>>>>>>>>>>>>>>>>> Installing Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
 yum install nginx -y
-echo ">>>>>>>>>>>>>>>>>>>>> Enabiling Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
+echo ">>>>>>>>>>>>>>>>>>>>> Enabiling Starting Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
 systemctl enable nginx
-echo ">>>>>>>>>>>>>>>>>>>>> Starting Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
 systemctl start nginx
 
-echo ">>>>>>>>>>>>>>>>>>>>> Removing junk files <<<<<<<<<<<<<<<<<<<<<<"
 rm -rf /usr/share/nginx/html/*
-echo ">>>>>>>>>>>>>>>>>>>>> Roboshop Artifacts <<<<<<<<<<<<<<<<<<<<<<"
+echo ">>>>>>>>>>>>>>>>>>>>> Downloading roboshop files <<<<<<<<<<<<<<<<<<<<<<"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo ">>>>>>>>>>>>>>>>>>>>> Entering in to repository <<<<<<<<<<<<<<<<<<<<<<"
+echo ">>>>>>>>>>>>>>>>>>>>> Extracting and unzipping <<<<<<<<<<<<<<<<<<<<<<"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo ">>>>>>>>>>>>>>>>>>>>> Enabiling Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
+echo ">>>>>>>>>>>>>>>>>>>>> Enabiling Restarting Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
 systemctl enable nginx
-echo ">>>>>>>>>>>>>>>>>>>>> Restarting Nginx Server <<<<<<<<<<<<<<<<<<<<<<"
 systemctl restart nginx ; tail -f /var/log/messages
